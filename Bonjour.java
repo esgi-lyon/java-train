6a46c13f93763868947266e4482585c9097dfea8
Martin Soenen <martin.soenen@outlook.com>
2020-04-21 10:00:41 +0200

    Add .class

--- Bonjour.java
+++ Bonjour.java
@@ -3,6 +3,6 @@ public class Bonjour
 	static String message = "Bonjour tout le monde !";
 	public static void main(String[] args)
 	{
-		System.out.println(message)
+		System.out.println(message);
 	}
 }
