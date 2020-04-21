10a5baa4a8a464c322510d672250911640e6d292
Martin Soenen <martin.soenen@outlook.com>
2020-04-21 14:36:06 +0200

    Some tests

--- Bonjour.java
+++ Bonjour.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Bonjour
 {
@@ -61,6 +62,52 @@ public class Bonjour
 		matrice[0][0] = 99; // Stockage d'une valeur pos 0,0
 		int[][] matrice2 = {{1,2,3},{4,5,6}};
 		System.out.println(Arrays.deepToString(matrice2));
+		// Manipulation de tableaux
+		System.out.println("La matrice comporte " + matrice2.length + " tableaux sur " + matrice2[0].length + " cases.");
+		// Création du tableau1 pour utiliser binarySearch
+		Integer[] tableau1 = {1234,888,986,678,564,123,789,352,987};
+		Arrays.sort(tableau1); // tri du tableau
+		// Recherche d'une valeur dans le tableau
+		System.out.println(Arrays.binarySearch(tableau1, 123));
+		System.out.println(Arrays.toString(tableau1));
+		for (int i=0 ; i<tableau1.length; i++) {
+			System.out.println(tableau1[i] + "\t");
+		}
+		// Inversion du tableau
+		Arrays.sort(tableau1, Collections.reverseOrder());
+		System.out.println(Arrays.deepToString(tableau1));
+		// Copie d'un tableau
+		Integer[] copieTableau1 = Arrays.copyOf(tableau1, tableau1.length);
+		Arrays.sort(copieTableau1);
+		System.out.println(Arrays.toString(copieTableau1));
+		// Copie d'une partie d'un tableau
+		Integer[] copieTableauPart3;
+		copieTableauPart3 = Arrays.copyOfRange(copieTableau1, 0, 3);
+		System.out.println(Arrays.toString(copieTableauPart3));
+		// Méthode fill
+		Arrays.fill(copieTableau1, 9);
+		System.out.println(Arrays.toString(copieTableau1));
+		// Chaines de caractères
+		String chaine1 = "chaine";
+		String chaine2 = new String("chaine");
+		chaine1 = "il a dit : \"ça suffit !\"";
+		// Utilisation mode length
+		System.out.println("La chaine1 contient " + chaine1.length() + " caractères.");
+		// Test de concaténation
+		chaine1 += " test";
+		System.out.println(chaine1);
+		// Utilisation de charAt
+		System.out.println("Le troisième caractère de la chaine est " + chaine1.charAt(0));
+		// Utilisation de la méthode substring
+		System.out.println("Un morceau de la chaine : " + chaine1.substring(5, chaine1.length()));
+		// Utilisation de equals
+		if(chaine1.equals(chaine2)){
+			System.out.println("Les chaines sont identiques");
+		} else {
+			System.out.println("Les chaines sont différents");
+		}
+		String chaine3 = "ma chaine 3";
+		String chaine4 = "ma ChainE 3";
 	}
 }
 
